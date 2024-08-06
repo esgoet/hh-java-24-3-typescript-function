263b283d375234c214a84154e5e16eed2d9036b8
esgoet <toddesgo@gmail.com>
2024-08-06 15:12:56 +0200

    add array practice

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+const numbers : number[] = [2,4,6,2,3,11,59];
+const doubledNumbers : number[] = numbers.map((number) => number * 2);
+const sum : number = numbers.reduce((a,b) => a + b);
+const hasGreaterThan10 : boolean = numbers.some((number) => number > 10);
+console.log(numbers);
+console.log(doubledNumbers);
+console.log(sum);
+console.log("List has at least one number greater than 10: " + hasGreaterThan10);
+
+const words : string[] = ["Hello", "Dolphin", "Minister","Fish"];
+const longWords : string[] = words.filter((word) => word.length > 5);
+console.log(words);
+console.log(longWords);
+
 
